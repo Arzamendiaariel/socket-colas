24b19d683e63f651bfba19c91b953e9418bf0f7d
Ariel Arzamendia <arzamendiaariel@gmail.com>
2022-07-02 23:23:30 -0300

    audio

--- public/js/publico.js
+++ public/js/publico.js
@@ -16,26 +16,31 @@ const lblEscritorio4 = document.querySelector('#lblEscritorio4');
 const socket = io();
 
 socket.on('estado-actual', (payload) =>{ 
+
+   //AUDIO: reproducir en firefox y habilitar la reporodcucción automatica de audio
+   const audio = new Audio('./audio/new-ticket.mp3');
+   audio.play();
+
    const [ticket1, ticket2, ticket3, ticket4] = payload
 
- if( ticket1 ){
-    lblTicket1.innerText     = 'Ticket ' + ticket1.numero;
-    lblEscritorio1.innerText =  ticket1.escritorio;
-    }
+   if( ticket1 ){
+      lblTicket1.innerText     = 'Ticket ' + ticket1.numero;
+      lblEscritorio1.innerText =  ticket1.escritorio;
+      }
 
- if( ticket2 ){
-    lblTicket2.innerText     = 'Ticket ' + ticket2.numero;
-    lblEscritorio2.innerText =  ticket2.escritorio;
-    }
+   if( ticket2 ){
+      lblTicket2.innerText     = 'Ticket ' + ticket2.numero;
+      lblEscritorio2.innerText =  ticket2.escritorio;
+      }
 
- if( ticket3 ){
-    lblTicket3.innerText     = 'Ticket ' + ticket3.numero;
-    lblEscritorio3.innerText =  ticket3.escritorio;
-    }
+   if( ticket3 ){
+      lblTicket3.innerText     = 'Ticket ' + ticket3.numero;
+      lblEscritorio3.innerText =  ticket3.escritorio;
+      }
 
- if( ticket4 ){
-    lblTicket4.innerText     = 'Ticket ' + ticket4.numero;
-    lblEscritorio4.innerText =  ticket4.escritorio;
+   if( ticket4 ){
+      lblTicket4.innerText     = 'Ticket ' + ticket4.numero;
+      lblEscritorio4.innerText =  ticket4.escritorio;
     }
 
 });
